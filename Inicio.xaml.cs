028ad7fb4e17578582c24b00cd19aab341bebd2c
Juanjov04 <juan.villalba.ruiz@udla.edu.ec>
2024-12-01 22:42:13 -0500

    Cambios agregar Inicio y notas

---
+++ Inicio.xaml.cs
@@ -0,0 +1,9 @@
+namespace JuanJoseVillalbaRuiz;
+
+public partial class Inicio : ContentPage
+{
+	public Inicio()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Notas.xaml.cs
@@ -0,0 +1,9 @@
+namespace JuanJoseVillalbaRuiz;
+
+public partial class Notas : ContentPage
+{
+	public Notas()
+	{
+		InitializeComponent();
+	}
+}
