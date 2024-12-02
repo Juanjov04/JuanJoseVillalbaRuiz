a230e2280950c2d26e3475e76320b760210ddd51
Juanjov04 <juan.villalba.ruiz@udla.edu.ec>
2024-12-01 23:12:18 -0500

    base de datos

---
+++ NotesDatabase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static JuanJoseVillalbaRuiz.Notas;
+
+namespace JuanJoseVillalbaRuiz
+{
+    public class NotesDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public NotesDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Note>().Wait();
+        }
+
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return _database.Table<Note>().ToListAsync();
+        }
+
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return _database.Table<Note>().Where(n => n.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.Id != 0)
+                return _database.UpdateAsync(note);
+            else
+                return _database.InsertAsync(note);
+        }
+
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return _database.DeleteAsync(note);
+        }
+    }
+}
